c72082b71eecc0a6225bce3673a55514edfc7bc6
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:57:25 +0700

    Update BootScene.ts

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -1,1 +1,17 @@
+import { Scene } from 'phaser';
 
+export class BootScene extends Scene {
+  constructor() {
+    super({ key: 'BootScene' });
+  }
+  
+  preload() {
+    this.load.setPath('assets');
+    // Placeholder loading, you can add actual assets here
+    this.load.image('dummy', 'placeholder.png');
+  }
+  
+  create() {
+    this.scene.start('PreloadScene');
+  }
+}
