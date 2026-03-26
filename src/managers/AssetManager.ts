67889155b5c788ae396d531ba9d165265384bca2
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:46:09 +0700

    Update AssetManager.ts

--- src/managers/AssetManager.ts
+++ src/managers/AssetManager.ts
@@ -1,1 +1,10 @@
-
+export class AssetManager {
+  static async loadImage(scene: Phaser.Scene, key: string, url: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      scene.load.image(key, url);
+      scene.load.once(`filecomplete-image-${key}`, () => resolve());
+      scene.load.once(`loaderror`, () => reject());
+      scene.load.start();
+    });
+  }
+}
