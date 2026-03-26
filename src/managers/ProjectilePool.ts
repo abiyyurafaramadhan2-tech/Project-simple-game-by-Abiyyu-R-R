31a973e41f3cf5d5971061b8a64698398ba8523b
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 21:06:44 +0700

    Update ProjectilePool.ts

--- src/managers/ProjectilePool.ts
+++ src/managers/ProjectilePool.ts
@@ -8,14 +8,14 @@ export class ProjectilePool {
   constructor(scene: Phaser.Scene, initialSize: number = 20) {
     this.scene = scene;
     for (let i = 0; i < initialSize; i++) {
-      this.pool.push(new Projectile(scene, 0, 0, new Phaser.Math.Vector2(0, 0), 0, 0, 'player'));
+      this.pool.push(new Projectile(scene, 0, 0));
     }
   }
   
   fire(config: IProjectileConfig): Projectile | null {
     let projectile = this.pool.find(p => !p.active);
     if (!projectile) {
-      projectile = new Projectile(this.scene, 0, 0, new Phaser.Math.Vector2(0, 0), 0, 0, 'player');
+      projectile = new Projectile(this.scene, 0, 0);
       this.pool.push(projectile);
     }
     projectile.fire(config.x, config.y, config.direction, config.speed, config.damage, config.owner);
