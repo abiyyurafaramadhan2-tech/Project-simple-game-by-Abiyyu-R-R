15d94b41d7e03b0d6913708dbb2e232221c82925
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:57:43 +0700

    Update PreloadScene.ts

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -1,1 +1,20 @@
+import { Scene } from 'phaser';
 
+export class PreloadScene extends Scene {
+  constructor() {
+    super({ key: 'PreloadScene' });
+  }
+  
+  preload() {
+    this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Loading...', { fontSize: '32px', color: '#fff' }).setOrigin(0.5);
+    // Generate texture for flares
+    const graphics = this.make.graphics({ x: 0, y: 0, add: false });
+    graphics.fillStyle(0xffffff, 1);
+    graphics.fillCircle(8, 8, 8);
+    graphics.generateTexture('flares', 16, 16);
+  }
+  
+  create() {
+    this.scene.start('MainScene');
+  }
+}
