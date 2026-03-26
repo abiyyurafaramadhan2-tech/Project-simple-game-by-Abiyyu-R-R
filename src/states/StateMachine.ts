6d33b71e6289a1d595e86d3b5e81836f3ad02db3
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:53:13 +0700

    Update StateMachine.ts

--- src/states/StateMachine.ts
+++ src/states/StateMachine.ts
@@ -1,1 +1,27 @@
-
+export class StateMachine {
+  private states: Map<string, any> = new Map();
+  private currentState: any = null;
+  private context: any;
+  
+  constructor(context: any) {
+    this.context = context;
+  }
+  
+  addState(name: string, state: any) {
+    this.states.set(name, state);
+  }
+  
+  changeState(name: string) {
+    if (this.currentState) this.currentState.onExit();
+    this.currentState = this.states.get(name);
+    if (this.currentState) this.currentState.onEnter();
+  }
+  
+  update(dt: number) {
+    if (this.currentState) this.currentState.onUpdate(dt);
+  }
+  
+  isState(name: string): boolean {
+    return this.currentState === this.states.get(name);
+  }
+}
