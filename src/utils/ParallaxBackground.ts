1083522dc0565f2cfa4c4fe3ed9336c8b639584d
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:42:01 +0700

    Update ParallaxBackground.ts

--- src/utils/ParallaxBackground.ts
+++ src/utils/ParallaxBackground.ts
@@ -1,1 +1,24 @@
-
+export class ParallaxBackground {
+  private layers: Phaser.GameObjects.TileSprite[];
+  
+  constructor(scene: Phaser.Scene, keys: string[], speeds: number[]) {
+    this.layers = [];
+    keys.forEach((key, i) => {
+      const layer = scene.add.tileSprite(0, 0, scene.cameras.main.width, scene.cameras.main.height, key);
+      layer.setOrigin(0, 0);
+      layer.setScrollFactor(0);
+      layer.setDepth(-10 - i);
+      this.layers.push(layer);
+    });
+    this.layers.forEach((layer, i) => {
+      (layer as any).speed = speeds[i];
+    });
+  }
+  
+  update(camera: Phaser.Cameras.Scene2D.Camera) {
+    this.layers.forEach(layer => {
+      const speed = (layer as any).speed;
+      layer.tilePositionX = camera.scrollX * speed;
+    });
+  }
+}
