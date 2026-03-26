8bfd5fccf03397dea2dc76a9c575ace12baa0fd6
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 18:40:39 +0700

    Update Hitbox.ts

--- src/utils/Hitbox.ts
+++ src/utils/Hitbox.ts
@@ -1,1 +1,10 @@
+import { IHitbox } from '../types/interfaces';
 
+export class Hitbox {
+  static collide(a: IHitbox, b: IHitbox): boolean {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+  }
+}
